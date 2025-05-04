15043f8d5aabb8b16d5653732b1225f26824d414
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2025-05-04 15:12:17 -0600

    added engine instance support

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -6,3 +6,4 @@ lune run tests/standard-datastores-v1.lune.luau
 lune run tests/messaging-service-v1.lune.luau
 lune run tests/publish-place-v1.lune.luau
 lune run tests/luau-execution-v2.lune.luau
+lune run tests/instances-v2.lune.luau
